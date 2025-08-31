921d3550f15c005bf387337a1a792092e389f5d2
ken kagai <jibken80@gmail.com>
2025-08-31 19:36:51 +0300

    add styles in the index.css for the app.jsx component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,6 +2,8 @@ import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
+import UserProfile from './components/UserProfile'
+import App from './App.css'
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
       <Header />
       <MainContent />
       <Footer />
+      <div>
+        <UserProfile name='Ken' age={21} bio='Aspiring Front-End Developer' />
+      </div>
     </>
   )
 }
