d0732ddfd7a082bbc508f5411015b9a1c79b1151
ken kagai <jibken80@gmail.com>
2025-09-08 13:22:20 +0300

    add styles for the components

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer style={{ position: 'fixed', bottom: 0, width: '100%', backgroundColor: 'navy', color: 'grey', textAlign: 'center', padding: '10px' }}>
+        <p>&copy; 2023 City Lovers</p>
+        </footer>
+    )
+}
