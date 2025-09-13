e12e9da4d91d8c222ce3bc0cc49e7b194efb2244
ken kagai <jibken80@gmail.com>
2025-09-13 13:59:22 +0300

    last commit for the last directory for alx-react-app-new

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -4,10 +4,17 @@ import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 import Counter from './components/Counter'
+import ProfilePage from './components/ProfilePage'
+import UserContext from './UserContext'
 
 function App() {
+
+  const userData = { name: 'Jane Doe', email: 'jane.doe@example.com'};
   return (
     <>
+      <UserContext.Provider value>
+        <ProfilePage />
+      </UserContext.Provider>
       <Header />
       <MainContent />
       <Counter />
