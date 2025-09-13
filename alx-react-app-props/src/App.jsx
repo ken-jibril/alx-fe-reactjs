db21a627c0703e5b38892b7ae6f6b05ac0e4d8a1
ken kagai <jibken80@gmail.com>
2025-09-13 13:57:47 +0300

    add a context api for the UserProfile to pass down data to it directly

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+import { UserContext } from './components/UserContext'
 import WelcomeMessage from './components/WelcomeMessage'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
@@ -5,17 +7,24 @@ import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
 
 function App() {
+  
+  const [userData] = useState({
+    name: 'Ken',
+    age: 21,
+    bio: 'Aspiring Front-End engineer.',
+  })
+
   return (
-    <>
+    <UserContext.Provider value={userData}>
       <Header />
       <MainContent />
       <Footer />
       <div>
-        <UserProfile name='Alice' age={25} bio='Loves hiking and photography' />
-        <UserProfile name='Ken' age={21} bio='Aspiring Front-End engineer.' />
+        {/* No need to pass props manually anymore! */}
+        <UserProfile />
       </div>
       {/* <WelcomeMessage /> */}
-    </>
+    </UserContext.Provider>
   )
 }
 
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,11 +1,16 @@
-export default function UserProfile(props) {
+import { useContext } from 'react'
+import { UserContext } from './UserContext'
 
-        return (
-            <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-                <h2 style={{ color: 'blue' }}>Name: {props.name}</h2>
-                <p>Age: <span style={{ fontWeight: 'bold' }}></span>{props.age}</p>
-                <p>Bio: {props.bio}</p>
-            </div>
-        )
-    }
- 
+function UserProfile() {
+  const { name, age, bio } = useContext(UserContext)
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+      <p>Bio: {bio}</p>
+    </div>
+  )
+}
+
+export default UserProfile
