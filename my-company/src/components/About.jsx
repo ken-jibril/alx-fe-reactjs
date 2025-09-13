c3912515f80efa563730527e6458247e2e5f424f
ken kagai <jibken80@gmail.com>
2025-09-13 20:53:58 +0300

    add images to home and about pages for more info about the company

--- my-company/src/components/About.jsx
+++ my-company/src/components/About.jsx
@@ -3,6 +3,12 @@
        <div style={{ padding: '20px' }}>
          <h1>About Us</h1>
          <p style={{ fontSize: '2em'}}>Our company has been providing top-notch services since 1990. We specialize in various fields including technology, marketing, and consultancy.</p>
+         <div className="images" style={{ display: 'flex', justifyContent: 'center', gap: '20px', marginTop: '20px', flexWrap: 'wrap', alignItems: 'center' }}>
+            <img src="https://images.unsplash.com/photo-1627560985113-ab67e8031f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fHRlY2hub2xvZ3klMjBoYWxsJTIwd2l0aCUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+            <img src="https://images.unsplash.com/photo-1627560985113-ab67e8031f40?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fHRlY2hub2xvZ3klMjBoYWxsJTIwd2l0aCUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+            <img src="https://images.unsplash.com/photo-1592669282789-cf5eac5807e5?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fHRlY2hub2xvZ3klMjBoYWxsJTIwd2l0aCUyMHBlb3BsZXxlbnwwfHwwfHx8MA%3D%3D" alt="" />
+
+         </div>
        </div>
      );
    }
--- my-company/src/components/Home.jsx
+++ my-company/src/components/Home.jsx
@@ -3,6 +3,10 @@ export default function Home() {
         <div style={{ padding: '20px' }}>
             <h1>Welcome to Our Company</h1>
             <p style={{ fontSize: '2em'}}>We are dedicated to delivering excellence in all our services.</p>
+            <div className="photo">
+                <img src="https://images.unsplash.com/photo-1708192548842-2d8babc7ddd3?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fHRlY2glMjBjb21wYW55JTIwYnVpbGRpbmd8ZW58MHx8MHx8fDA%3D" alt="" style={{borderRadius: '5PX'}}/>
+                <p style={{ fontSize: '1.5em'}}>Our Headquarters are located at Bloemfontein, SouthAfrica.</p>
+            </div>
         </div>
     )
 }
