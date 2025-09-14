3b634bc4373953365b47b0ba8794d8958b4b59aa
ken kagai <jibken80@gmail.com>
2025-09-14 19:12:29 +0300

    add a background color for the navigation

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
     return (
-        <nav style={{ background: 'navy', color: 'navy', display: 'flex', flexDirection: 'row', justifyContent: 'center', padding: '15px', fontSize: '2em', margin: '0', textDecoration: 'none'}}>
+        <nav style={{ backgroundColor: 'navy', color: 'navy', display: 'flex', flexDirection: 'row', justifyContent: 'center', padding: '15px', fontSize: '2em', margin: '0', textDecoration: 'none'}}>
             <Link to="/" style={{color: 'white'}}>Home</Link> {" "}
           | <Link to="/about" style={{color: 'white'}}>About</Link> {" "}
           | <Link to="/services" style={{color: 'white'}}>Services</Link>{" "}
