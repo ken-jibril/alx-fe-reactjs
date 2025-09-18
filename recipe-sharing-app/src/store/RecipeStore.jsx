9e09c727ffeaa3fb03a9b6f3c5babc2d78df24ca
ken kagai <jibken80@gmail.com>
2025-09-18 11:29:57 +0300

    remove the store folder and its file from the directory

--- recipe-sharing-app/src/store/RecipeStore.jsx
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-
-const useRecipeStore = create((set) => ({
-    recipes: [],
-    addRecipe: (newRecipes) => set((state) => ({ recipes: [...state.recipes, newRecipes ] })),
-    setRecipes: (newRecipes) => set({ recipes: newRecipes }),
-}))
-
-export default useRecipeStore;
