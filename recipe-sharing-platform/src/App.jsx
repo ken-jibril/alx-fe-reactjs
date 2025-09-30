266979e80127bfb5b5ea596badfe2005e0d05eac
ken kagai <jibken80@gmail.com>
2025-09-30 14:06:07 +0300

    import the HomePage and render it

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
+import HomePage from './components/HomePage'
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className="text-blue-700 text-3xl text-center">Hello there from Tailwind CCS!</h1>
+      <HomePage />
     </>
   )
 }
