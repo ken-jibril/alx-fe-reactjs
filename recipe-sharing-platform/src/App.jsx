07dfa5ce8643828a33376c60516b9c1513b59910
ken kagai <jibken80@gmail.com>
2025-09-30 15:11:17 +0300

    import react router dom for navigating through the site

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,13 +1,17 @@
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { useState } from 'react'
 import HomePage from './components/HomePage'
-
+import RecipeDetail from './components/RecipeDetail'
 
 function App() {
 
   return (
-    <>
-      <HomePage />
-    </>
+    <Router>
+       <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipes/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
   )
 }
 
