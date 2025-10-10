198242f1983bbd8d9c9675b3c6b6b628c304e7ad
ken kagai <jibken80@gmail.com>
2025-10-10 15:03:11 +0300

    Import RegistrationForm component into App

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-
+import RegistrationForm from './components/RegistrationForm'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -7,6 +7,7 @@ function App() {
   return (
     <>
        <h1>Hello World🖐</h1>
+        <RegistrationForm />
     </>
   )
 }
