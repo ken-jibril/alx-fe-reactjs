c427f07aa962b9c8fb0c056e7807d8c64eab6b03
ken kagai <jibken80@gmail.com>
2025-10-12 15:56:01 +0300

    import post component from the component folder

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import PostsComponent from './PostsComponent';
+import PostsComponent from './components/PostsComponent';
 
 const queryClient = new QueryClient();
 
