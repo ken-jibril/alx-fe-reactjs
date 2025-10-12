621a773344fccb29928bf280ea1158120ab434a5
ken kagai <jibken80@gmail.com>
2025-10-12 17:38:15 +0300

    import a todo list component and render it

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import TodoList from './components/TodoList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Hello there</h1>
+      <TodoList />
     </>
   )
 }
