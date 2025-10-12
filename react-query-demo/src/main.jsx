a38a8cd13b3025bbc9e8284b1879cb71b1094b7c
ken kagai <jibken80@gmail.com>
2025-10-12 15:54:39 +0300

    return the default code.

--- react-query-demo/src/main.jsx
+++ react-query-demo/src/main.jsx
@@ -1,15 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </StrictMode>,
-);
+)
