a53e29ebc14862b6b2cd8d23b555e394d7620fa0
ken kagai <jibken80@gmail.com>
2025-10-12 15:40:39 +0300

    remove importation of react query and return the previous code.

--- form-handling-react/src/main.jsx
+++ form-handling-react/src/main.jsx
@@ -1,22 +1,10 @@
-import React, { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import App from './App.jsx';
-import './index.css';
-
-// create the React Query client
-const queryClient = new QueryClient();
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-      {/* optional devtools for cache debugging */}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
